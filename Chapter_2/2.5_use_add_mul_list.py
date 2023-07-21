3cb7c14996f295820b07e5c8b354e7826a71b981
FEIFEI SUN <treasuresff@gmail.com>
2023-07-21 13:00:20 +0800

    FEAT: add code note 2023/07/21;

--- Chapter_2/2.5_use_add_mul_list.py
+++ Chapter_2/2.5_use_add_mul_list.py
@@ -5,7 +5,22 @@ Detail:
 Date: 2023-07-20 17:26:06
 
 '''
+def new_list_ref():
+    print("创建不含引用的列表")
+    board = [['_'] * 3 for i in range(3)]
+    print(board)
+    board[1][2] = 'X'
+    print(board)
 
+    
+    print("-----含有3个指向同一对象的引用的列表是毫无用处的-----")
+    weird_board = [['_'] * 3] * 3
+    print(weird_board)
+    
+    print("想要标记第一行第二列的元素时，马上暴露出列表内的3个引用指向同一个对象的事实")
+    weird_board[1][2] = 'X'
+    print(weird_board)
+    
 if __name__ == '__main__':   
 
     print("将一个序列复制几份再拼接起来，更快的方法是乘法")
@@ -15,3 +30,8 @@ if __name__ == '__main__':
     print("拼接和乘操作，都会产生一个新序列")
     
     print(5 * 'abcd ')
+    
+    new_list_ref()
+    
+    
+    
